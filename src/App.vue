3d4ccc8f064d2f01a5b1050be4fab26196770be9
Kevin Elberger <kevelberger@gmail.com>
2020-12-10 01:21:23 -0800

    Init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,57 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="controls">
+    <div class="keys-list">
+      <Key :text="'⇦'" />
+      <Key :text="'⇧'" />
+      <Key :text="'⇩'" />
+      <Key :text="'⇨'" />
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Key from '@/components/Key.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Key
   }
-}
+};
 </script>
 
 <style>
+*, *:before, *:after {
+  box-sizing: border-box;
+}
+
+body, html {
+  margin: 0;
+  padding: 0;
+}
+
 #app {
+  width: 100%;
+  height: 100vh;
+  overflow: hidden;
+  background: #0d1117;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+.controls {
+  bottom: 0;
+  width: 100%;
+  position: fixed;
+  padding: 24px 16px 48px;
+  border-top: 1px solid #21262d;
+}
+
+.keys-list {
+  column-gap: 8px;
+  display: inline-grid;
+  grid-template-columns: repeat(4, 2.75rem);
 }
 </style>
---
+++ src/components/Key.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="key">{{text}}</div>
+</template>
+
+<script>
+export default {
+  name: 'Key',
+  props: ['text']
+}
+</script>
+
+<style scoped>
+.key {
+  color: #8b949e;
+  cursor: pointer;
+  font-weight: 700;
+  display: inline-flex;
+  font-size: 1.15rem;
+  min-width: 2.75rem;
+  height: 2.75rem;
+  border-radius: 6px;
+  align-items: center;
+  justify-content: center;
+  font-family: Inter, sans-serif;
+  background-color: #161b22;
+}
+
+.key:hover {
+  color: #58a6ff;
+}
+</style>
